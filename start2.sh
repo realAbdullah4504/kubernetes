f968641d4e60947021ffbca2d6c238a6b9f8ebf1
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-01-18 12:33:07 +0500

    ingress

--- start2.sh
+++ start2.sh
@@ -1,97 +1,74 @@
 #!/bin/bash
-          # # Log all output to a file
-          # sudo cat /var/log/user-data.log
-          # # sudo cat /var/log/user-data.log
-          # exec > /var/log/user-data.log 2>&1
-          set -x  # Enable script debugging (print each command before executing)
+# # Log all output to a file
+# sudo cat /var/log/user-data.log
+# # sudo cat /var/log/user-data.log
+# exec > /var/log/user-data.log 2>&1
+set -x  # Enable script debugging (print each command before executing)
 
-          # Disable swap
-          swapoff -a
-          sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+# Disable swap
+swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-          # Load required kernel modules
-          cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-          overlay
-          br_netfilter
-          EOF
+# Load required kernel modules
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
 
-          sudo modprobe overlay
-          sudo modprobe br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-          # Set sysctl parameters for Kubernetes networking
-          cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-          net.bridge.bridge-nf-call-iptables  = 1
-          net.bridge.bridge-nf-call-ip6tables = 1
-          net.ipv4.ip_forward                 = 1
-          EOF
+# Set sysctl parameters for Kubernetes networking
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
 
-          sudo sysctl --system
+sudo sysctl --system
 
-          # Verify kernel modules and sysctl parameters
-          lsmod | grep br_netfilter || echo "br_netfilter module not loaded"
-          lsmod | grep overlay || echo "overlay module not loaded"
-          sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward || echo "Sysctl parameters not set correctly"
+# Verify kernel modules and sysctl parameters
+lsmod | grep br_netfilter || echo "br_netfilter module not loaded"
+lsmod | grep overlay || echo "overlay module not loaded"
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward || echo "Sysctl parameters not set correctly"
 
-          # Install containerd
-          curl -LO https://github.com/containerd/containerd/releases/download/v1.7.14/containerd-1.7.14-linux-amd64.tar.gz
-          sudo tar Cxzvf /usr/local containerd-1.7.14-linux-amd64.tar.gz
-          curl -LO https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-          sudo mkdir -p /usr/local/lib/systemd/system/
-          sudo mv containerd.service /usr/local/lib/systemd/system/
-          sudo mkdir -p /etc/containerd
-          containerd config default | sudo tee /etc/containerd/config.toml
-          sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-          sudo systemctl daemon-reload
-          sudo systemctl enable --now containerd
+# Install containerd
+curl -LO https://github.com/containerd/containerd/releases/download/v1.7.14/containerd-1.7.14-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local containerd-1.7.14-linux-amd64.tar.gz
+curl -LO https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo mkdir -p /usr/local/lib/systemd/system/
+sudo mv containerd.service /usr/local/lib/systemd/system/
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
 
-          # Verify containerd service is running
-          if ! systemctl status containerd; then
-            echo "Containerd service failed to start!"
-          fi
+# Verify containerd service is running
+if ! systemctl status containerd; then
+echo "Containerd service failed to start!"
+fi
 
-          # Install runc
-          curl -LO https://github.com/opencontainers/runc/releases/download/v1.1.12/runc.amd64
-          sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+# Install runc
+curl -LO https://github.com/opencontainers/runc/releases/download/v1.1.12/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 
-          # Install CNI plugins
-          curl -LO https://github.com/containernetworking/plugins/releases/download/v1.5.0/cni-plugins-linux-amd64-v1.5.0.tgz
-          sudo mkdir -p /opt/cni/bin
-          sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.5.0.tgz
+# Add Kubernetes package repository and install kubelet, kubeadm, kubectl
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet=1.29.6-1.1 kubeadm=1.29.6-1.1 kubectl=1.29.6-1.1 --allow-downgrades --allow-change-held-packages
+sudo apt-mark hold kubelet kubeadm kubectl
 
-          # Add Kubernetes package repository and install kubelet, kubeadm, kubectl
-          sudo apt-get update
-          sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-          sudo mkdir -p /etc/apt/keyrings
-          curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-          echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-          sudo apt-get update
-          sudo apt-get install -y kubelet=1.29.6-1.1 kubeadm=1.29.6-1.1 kubectl=1.29.6-1.1 --allow-downgrades --allow-change-held-packages
-          sudo apt-mark hold kubelet kubeadm kubectl
+echo "Initializing Kubernetes cluster..."
+sudo apt install kubectx
 
-          #configure crictl
-          sudo crictl config runtime-endpoint unix:///var/run/containerd/containerd.sock
+echo "Disabling AppArmor..."
+sudo systemctl stop apparmor && sudo systemctl disable apparmor
 
-          # init kubeadm
-          sudo kubeadm init --pod-network-cidr=192.168.0.0/16
-
-          mkdir -p $HOME/.kube
-          sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-          sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-          # Install Calico CNI
-          kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-          curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
-          kubectl apply -f custom-resources.yaml
-
-          echo "Initializing Kubernetes namespace..."
-          sudo apt install kubectx
-
-          echo "Disabling AppArmor..."
-          sudo systemctl stop apparmor && sudo systemctl disable apparmor
-
-          # Add kubectl alias to .bashrc
-          echo "alias k='kubectl'" >> /home/ubuntu/.bashrc
-          echo "export EDITOR=nano" >> /home/ubuntu/.bashrc
-
-          # Reload .bashrc
-          source /home/ubuntu/.bashrc
+kubeadm join 172.31.15.207:6443 --token 1bi4y9.yxr9u4q1tkjrcoa4 --discovery-token-ca-cert-hash sha256:fcaf96551ee723ef8c8a802dfdfb2db3d78db6e65dbc9634be03c59ad50e1d76
+mkdir -p /root/.kube 
+sudo cp /etc/kubernetes/kubelet.conf /root/.kube/config
